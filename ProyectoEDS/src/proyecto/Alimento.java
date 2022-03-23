f7315dfaa1380220fb374ac3d10216b55c60943f
Alejandro-VP <alejandrovp1733@gmail.com>
2022-03-23 12:47:04 +0100

    Inicio de la clase Alimento.
    Se han definido los posibles atributos como comienzo del proyecto.
    
    Signed-off-by: Alejandro-VP <alejandrovp1733@gmail.com>

---
+++ ProyectoEDS/src/proyecto/Alimento.java
@@ -0,0 +1,13 @@
+package proyecto;
+
+public class Alimento {
+protected String nombre;
+protected double grasas;
+protected double grasasSat;
+protected double hidratosCarbono;
+protected double azucar;
+protected double proteinas;
+protected double sal;
+protected double calorías;
+	
+}
