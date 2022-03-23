c38e285edd0dfa13e2fd43e4724b7ce34385dbe1
Alejandro-VP <alejandrovp1733@gmail.com>
2022-03-23 13:59:57 +0100

    Primer prototipo interfaz gráfica
    Hemos colaborado los 3 en el primer posible diseño de la interfaz
    gráfica, añadiendo etiquetas, cajas de texto y lo que serían las
    "celdas" de los resultados
    
    
    Signed-off-by: Alejandro-VP <alejandrovp1733@gmail.com> & Alejandro Moya Peris & Karen Lizeth Restrepo Villegas

--- ProyectoEDS/src/proyecto/Alimento.java
+++ ProyectoEDS/src/proyecto/Alimento.java
@@ -8,6 +8,83 @@ protected double hidratosCarbono;
 protected double azucar;
 protected double proteinas;
 protected double sal;
-protected double calorías;
+protected double calorias;
 	
+Alimento (String nombre, double grasas, double grasasSat, double hidratosCarbono, double azucar, double proteinas, double sal, double calorías){
+	this.nombre = nombre;
+	this.grasas = grasas;
+	this.grasasSat = grasasSat;
+	this.hidratosCarbono = hidratosCarbono;
+	this.azucar = azucar;
+	this.proteinas = proteinas;
+	this.sal = sal;
+	this.calorias = calorias;
+}
+
+public String getNombre() {
+	return nombre;
+}
+
+public double getGrasas() {
+	return grasas;
+}
+
+public double getGrasasSat() {
+	return grasasSat;
+}
+
+public double getHidratosCarbono() {
+	return hidratosCarbono;
+}
+
+public double getAzucar() {
+	return azucar;
+}
+
+public double getProteinas() {
+	return proteinas;
+}
+
+public double getSal() {
+	return sal;
+}
+
+public double getCalorias() {
+	return calorias;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
+public void setGrasas(double grasas) {
+	this.grasas = grasas;
+}
+
+public void setGrasasSat(double grasasSat) {
+	this.grasasSat = grasasSat;
+}
+
+public void setHidratosCarbono(double hidratosCarbono) {
+	this.hidratosCarbono = hidratosCarbono;
+}
+
+public void setAzucar(double azucar) {
+	this.azucar = azucar;
+}
+
+public void setProteinas(double proteinas) {
+	this.proteinas = proteinas;
+}
+
+public void setSal(double sal) {
+	this.sal = sal;
+}
+
+public void setCalorias(double calorias) {
+	this.calorias = calorias;
+}
+
+
+
 }
